3aa3ed131595b8a6abfe61c50d1d833f325ac8ec
Paul Franklin <paul.franklin@csuglobal.edu>
2024-03-27 18:26:41 -0400

    pausing for now, phenomenal progress on this

--- src/PAF.java
+++ src/PAF.java
@@ -1,29 +1,26 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
 
 public class PAF extends Frame implements ActionListener {
     String title;
+    Button depButton;
+    Button withButton;
 
     public PAF (String title) {
-        this.title = title;
+        super();
         Button depButton = new Button("Deposit");
         Button withButton = new Button("Withdrawal");
         depButton.addActionListener(this);
-        withButton.addActionListener(this);
+        withButton.addActionListener( this);
     }
 
-
-
-    //public void actionPerformed(ActionEvent e) {
-        //String text;
-        //int numClicks;
-        //text.setText("Button Clicked " + numClicks + " times");
-    //}
     public static void main(String[] args) {
-        PAF bankFrame = new PAF("bankTransactions");
+        PAF bankFrame = new PAF("Bank Transactions");
         bankFrame.setTitle("My Bank Account: Withdraw and Deposit");
-        bankFrame.setSize(800, 800);
+        bankFrame.setSize(800, 100);
         bankFrame.setVisible(true);
 
         JPanel transactionPanel = new JPanel();
@@ -32,5 +29,11 @@ public class PAF extends Frame implements ActionListener {
         bankFrame.add(transactionPanel);
 
 
-    }}
+    }
+
+    @Override
+    public void actionPerformed(java.awt.event.ActionEvent e) {
+    }
+    }
+
 
