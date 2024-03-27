f623009c61f0217d224c478ece35f6523d2a3b95
Paul Franklin <paul.franklin@csuglobal.edu>
2024-03-27 15:51:38 -0400

    first save

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+public class Main {
+    public static void main(String[] args) {
+                // Create a new JFrame
+        JFrame bankFrame = new JFrame();
+
+                // Set the title of the frame
+        bankFrame.setTitle("My Bank Account: Withdraw and Deposit");
+
+                // Set the size of the frame
+        bankFrame.setSize(800, 300);
+
+                // Make the frame visible
+        bankFrame.setVisible(true);
+            }
+        }
